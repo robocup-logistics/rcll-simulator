13c498987b2f7daec6693ab81c78cc91fdc7462c
Tarik Viehmann <viehmann@kbsg.rwth-aachen.de>
2022-12-11 15:13:01 +0100

    get refbox info via tcp socket
    The tcp stream socket is for controller level exchange, it allows to
    send messages to  switch phases, pause or otherwise manipulate the
    refbox. More importantly, the refbox always sends full ground-truth over
    the tcp socket, wich is exactly what the mps manager needs.

--- Simulator/Configurations.cs
+++ Simulator/Configurations.cs
@@ -359,7 +359,7 @@ namespace Simulator
         private static RefboxConfig? CreateRefboxConfig(YamlMappingNode refbox)
         {
             string? ip = null;
-            int publicSendPort = 0, publicRecvPort = 0, cyanSendPort = 0, cyanRecvPort = 0, magentaSendPort = 0, magentaRecvPort = 0;
+            int tcpPort = 0, publicSendPort = 0, publicRecvPort = 0, cyanSendPort = 0, cyanRecvPort = 0, magentaSendPort = 0, magentaRecvPort = 0;
             var children = refbox.Children;
             // Step into the public information
             //Console.WriteLine(children[0].Key.ToString());
@@ -376,6 +376,9 @@ namespace Simulator
                             {
                                 case "ip":
                                     ip = yamlNode1.ToString();
+                                    break;
+                                case "tcp":
+                                    tcpPort = int.Parse(yamlNode1.ToString());
                                     break;
                                 case "send":
                                     publicSendPort = int.Parse(yamlNode1.ToString());
@@ -437,7 +440,7 @@ namespace Simulator
             {
                 return null;
             }
-            var config = new RefboxConfig(ip, publicSendPort, publicRecvPort, cyanSendPort, cyanRecvPort,
+            var config = new RefboxConfig(ip, tcpPort, publicSendPort, publicRecvPort, cyanSendPort, cyanRecvPort,
                 magentaSendPort, magentaRecvPort);
             return config;
         }
@@ -515,6 +518,7 @@ namespace Simulator
     public class RefboxConfig
     {
         public string IP { get; }
+        public int TcpPort { get; }
         public int PublicSendPort { get; }
         public int PublicRecvPort { get; }
         public int CyanSendPort { get; }
@@ -522,9 +526,10 @@ namespace Simulator
         public int MagentaSendPort { get; }
         public int MagentaRecvPort { get; }
 
-        public RefboxConfig(string ip, int publicSend, int publicRecv, int cyanSend, int cyanRecv, int magentaSend, int magentaRecv)
+        public RefboxConfig(string ip,int tcpPort, int publicSend, int publicRecv, int cyanSend, int cyanRecv, int magentaSend, int magentaRecv)
         {
             IP = ip;
+            TcpPort = tcpPort;
             PublicRecvPort = publicRecv;
             PublicSendPort = publicSend;
             CyanRecvPort = cyanRecv;
@@ -536,6 +541,7 @@ namespace Simulator
         {
             Console.WriteLine("---------------------------");
             Console.WriteLine("Ip = [" + IP + "]");
+            Console.WriteLine("TcpPort = [" + TcpPort + "]");
             Console.WriteLine("PublicRecvPort = [" + PublicRecvPort + "]");
             Console.WriteLine("PublicSendPort = [" + PublicSendPort + "]");
             Console.WriteLine("CyanRecvPort = [" + CyanRecvPort + "]");
