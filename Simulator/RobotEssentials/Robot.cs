691292ab63e28e2939ccbef6ed136db791e398d1
Tim Wendt <tim.wendt@alumni.fh-aachen.de>
2025-04-28 20:15:41 +0200

    fixed dublicated task execution

--- Simulator/RobotEssentials/Robot.cs
+++ Simulator/RobotEssentials/Robot.cs
@@ -160,6 +160,10 @@ public partial class Robot {
                     MyLogger.Debug("Recived the current task again. Going to ignore that message");
                     return;
                 }
+                if (FinishedTasks.Any(task => task.TaskId == task.TaskId)){
+                    MyLogger.Debug("Recived a finished task again. Going to ignore that message");
+                    return;
+                }
                 MyLogger.Info("Received a new task!");
                 TaskMutex.ReleaseMutex();
                 if (!CancelCurrentTask(false)) {
